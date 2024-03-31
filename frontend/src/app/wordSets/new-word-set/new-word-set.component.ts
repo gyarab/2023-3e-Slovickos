f6bbd69bc96fd968132633b97d7bc80de8723b59
Vanacka <ivanmerkulov34@gmail.com>
2024-03-31 17:32:08 +0200

    Add constructor

--- frontend/src/app/wordSets/new-word-set/new-word-set.component.ts
+++ frontend/src/app/wordSets/new-word-set/new-word-set.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../word-set.service';
 
 @Component({
   selector: 'app-new-word-set',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class NewWordSetComponent {
 
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    const userId = this.dataService.getCurrentUserId();
+  }
 }
