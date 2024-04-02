552c5a800305fe7d06f75127d0ae9b3a5f996150
Brnobossicek <100611982+Brnobossicek@users.noreply.github.com>
2024-04-02 17:50:21 +0200

    get

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AccountComponent } from './account/account.component';
 import { AuthComponent } from './auth/auth.component';
 import { WordSetListComponent } from './wordSets/word-set-list/word-set-list.component';
 import { NewWordSetComponent } from './wordSets/new-word-set/new-word-set.component';
+import { WordSetDetailComponent } from './wordSets/word-set-detail/word-set-detail.component';
 
 const routes: Routes = [
   {path: 'profile', component: AccountComponent},
   {path: 'auth', component: AuthComponent},
   {path: 'word-sets', component: WordSetListComponent},
   {path: 'new', component: NewWordSetComponent},
+  {path: 'word', component: WordSetDetailComponent},
 ];
 
 @NgModule({
---
+++ frontend/src/app/wordSets/word-set-detail/word-set-detail.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WordSetDetailComponent } from './word-set-detail.component';
+
+describe('WordSetDetailComponent', () => {
+  let component: WordSetDetailComponent;
+  let fixture: ComponentFixture<WordSetDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [WordSetDetailComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(WordSetDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
