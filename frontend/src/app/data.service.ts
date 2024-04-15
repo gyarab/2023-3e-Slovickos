42775ca98fad2d3c0650c5313a54a5c9e469dcdd
Vanacka <ivanmerkulov34@gmail.com>
2024-04-15 14:01:13 +0200

    Make getCurrUserId function more globaly accesible

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -6,5 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  
+  getCurrentUserId(): number | null {
+    const userDataString = localStorage.getItem('user');
+    // Retrieve user data from localStorage
+    if (userDataString) {
+      // Parse user data string to an object
+      const userData = JSON.parse(userDataString);
+      
+      // Access the id property
+      return userData.id;
+    } 
+    else {
+      console.error('No user is logged in.');
+      return null;
+    }
+  }
 }
