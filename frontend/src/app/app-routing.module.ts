10d61b02b67dfca84303b7dda19fa33cf9eaadcb
Vanacka <ivanmerkulov34@gmail.com>
2024-04-15 13:57:09 +0200

    Add routing for home page to navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AuthComponent } from './auth/auth.component';
 import { WordSetListComponent } from './wordSets/word-set-list/word-set-list.component';
 import { NewWordSetComponent } from './wordSets/new-word-set/new-word-set.component';
 import { WordSetDetailComponent } from './wordSets/word-set-detail/word-set-detail.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'profile', component: AccountComponent},
   {path: 'auth', component: AuthComponent},
   {path: 'word-sets', component: WordSetListComponent},
-  {path: 'new', component: NewWordSetComponent},
+  {path: 'word-sets/new', component: NewWordSetComponent},
   {path: 'word-sets/:setid', component: WordSetDetailComponent},
+  {path: '', component: HomeComponent},
 ];
 
 @NgModule({
