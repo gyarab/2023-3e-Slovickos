a0f26b913bbe16134b2542ae31513bfd14beab91
Vanacka <ivanmerkulov34@gmail.com>
2024-04-21 23:15:05 +0200

    Update favicon.ico and add the route for updating user credentials

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountComponent } from './account/account.component';
+import { AccountComponent } from './account/show-account/account.component';
 import { AuthComponent } from './auth/auth.component';
 import { WordSetListComponent } from './wordSets/word-set-list/word-set-list.component';
 import { NewWordSetComponent } from './wordSets/new-word-set/new-word-set.component';
@@ -8,6 +8,7 @@ import { WordSetDetailComponent } from './wordSets/word-set-detail/word-set-deta
 import { NewWordComponent } from './wordSets/word-set-detail/new-word/new-word.component';
 import { LearningComponent } from './learning/learning.component';
 import { HomeComponent } from './home/home.component';
+import { UpdateAccountComponent } from './account/update-account/update-account.component';
 
 const routes: Routes = [
   {path: 'profile', component: AccountComponent},
@@ -18,6 +19,7 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'new-word', component: NewWordComponent},
   {path: 'learning/:setid', component: LearningComponent},
+  {path: 'profile/update', component: UpdateAccountComponent},
 ];
 
 @NgModule({
